5a83b2cae3042443b86200a332f38aff2438b6e7
mhassankhanw3 <mhassankhanw3@gmail.com>
2023-12-20 20:45:39 +0500

    fourth commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Host, Connect, Celebrate: Your Events, Our Platform!
+              Hosting, Connect, Celebrate: Your Events, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
               Book and learn helpful tips from 3,168+ mentors in world-class
@@ -36,8 +36,7 @@ export default function Home() {
           Trusted by <br /> Thousands of Events
         </h2>
         <div className="flex flex-col w-full gap-5 md:flex-row">
-          Search
-           Category
+          Search Category
         </div>
       </section>
     </>
